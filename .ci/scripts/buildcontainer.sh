0fa91bef16b38144226f37836664f78b86bbcea3
Mike Roth <mike.roth@assurant.com>
2018-11-29 17:49:51 -0800

    Update to test-branch * Added magic for pushing to container * Added magic to pods

--- .ci/scripts/buildcontainer.sh
+++ .ci/scripts/buildcontainer.sh
@@ -1,2 +1,7 @@
 #!/busybox/sh
-/kaniko/executor -f `pwd`/Dockerfile -c `pwd` --no-push
+MAJOR=1
+MINOR=0
+BUILD=$BUILD_ID
+VERSION=${MAJOR}.${MINOR}.${BUILD}
+
+/kaniko/executor -f `pwd`/Dockerfile -c `pwd` --destination=sspdevnettest.azurecr.io/ui/ui-npm/${BRANCH_NAME}:${VERSION}
